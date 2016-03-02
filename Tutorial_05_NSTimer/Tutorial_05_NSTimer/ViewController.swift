a1c3844c4f396c734668d650712a6a169c9bae0f
Truong Tuan Quang <tuanquanghpvn@gmail.com>
2016-03-02 13:42:47 +0700

    [ADD] Tutorial 05 NSTimer

---
+++ Tutorial_05_NSTimer/Tutorial_05_NSTimer/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Tutorial_05_NSTimer
+//
+//  Created by  on 3/2/16.
+//  Copyright © 2016 Tuan_Quang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var timer: NSTimer!
+    
+    @IBOutlet weak var lblShow: UILabel!
+    
+    @IBAction func btnStart(sender: UIButton) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("schedulerFun"), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func btnStop(sender: UIButton) {
+        timer.invalidate()
+    }
+    
+    func schedulerFun() {
+        let rs: String! = lblShow.text
+        if rs != nil {
+            let trs: Int! = Int(rs)! + 1
+            lblShow.text = String(trs)
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
