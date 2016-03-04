385a45bcf54408157c2a6eb23be774ec7680d62a
Truong Tuan Quang <tuanquanghpvn@gmail.com>
2016-03-04 11:17:37 +0700

    [ADD] Tutorial 16 Gesture

---
+++ Tutorial_16_Gessture/Tutorial_16_Gessture/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Tutorial_16_Gessture
+//
+//  Created by  on 3/4/16.
+//  Copyright © 2016 Tuan_Quang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func actGestureImage(sender: AnyObject) {
+        print("Hey! I am monkey")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
